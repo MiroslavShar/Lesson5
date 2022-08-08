0ec7eb23f951df5e10e013ec6b1e36401d249b65
Miroslav <miroslav.ievtushenko@gmail.com>
2022-08-08 19:26:11 +0200

    update 2.0 Lesson 5

--- src/Trenerovka.java
+++ src/Trenerovka.java
@@ -1,3 +1,23 @@
 public class Trenerovka {
-    
+    int a;
+    int b;
+    int c;
+
+    public static void main(String[] args) {
+        Trenerovka start = new Trenerovka(5, 5, 5);
+        //start.count();
+
+        System.out.println(start.count());
+    }
+
+    public Trenerovka(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int count() {
+        int result = a + b + c;
+        return result;
+    }
 }
